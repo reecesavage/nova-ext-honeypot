c155f9d2cb432919fe4a85b08e021a73101ee0bf
Reece <reece.savage@gmail.com>
2021-02-22 23:29:05 -0600

    Add files via upload

---
+++ views/main/js/custom.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+$('form').submit(function(){
+
+        if ($('input.totallynotconspicuous').val().length != 0) {
+            return false;
+        } 
+});
+
+
+
+
+
+});
---
+++ views/main/pages/form.php
@@ -0,0 +1,4 @@
+<label class="totallynotconspicuous" for="website"></label>
+<input class="totallynotconspicuous" autocomplete="off" type="text" id="website" name="Website" placeholder="Your website here">
+<label class="totallynotconspicuous" for="lastName"></label>
+<input class="totallynotconspicuous" autocomplete="off" type="text" id="lastName" name="Last Name" placeholder="Last Name">
